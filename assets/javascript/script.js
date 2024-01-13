30fb0dbacebc414fc46ff05059fddb41facf5c96
Dilek Nuredin <dileknuredin@hotmail.com>
2024-01-13 16:37:29 -0500

    updated media quaries on css

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,7 +2,7 @@ var toggler = document.getElementsByClassName("caret");
 var i;
 
 for (i = 0; i < toggler.length; i++) {
-  toggler[i].addEventListener("click", function() {
+  toggler[i].addEventListener("click", function () {
     this.parentElement.querySelector(".nested").classList.toggle("active");
     this.classList.toggle("caret-down");
   });
